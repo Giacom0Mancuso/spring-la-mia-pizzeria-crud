083b7e38f1dfe1edf70ec8efbff4620294d2b3cd
Giacom0Mancuso <giacomomancuso35@email.com>
2024-01-10 17:41:42 +0100

    changes

--- src/main/java/com/example/springlamiapizzeriacrud/controller/HomeController.java
+++ src/main/java/com/example/springlamiapizzeriacrud/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.example.springlamiapizzeriacrud.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.awt.print.Book;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -12,4 +18,6 @@ public class HomeController {
     public String home() {
         return "redirect:/menu";
     }
+
+
 }
--- src/main/java/com/example/springlamiapizzeriacrud/controller/MenuController.java
+++ src/main/java/com/example/springlamiapizzeriacrud/controller/MenuController.java
@@ -3,11 +3,16 @@ package com.example.springlamiapizzeriacrud.controller;
 import com.example.springlamiapizzeriacrud.model.Pizza;
 import com.example.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/menu")
@@ -22,7 +27,17 @@ public class MenuController {
         return "pizza/list";
     }
 
-
+    @GetMapping("/show/{id}") // /books/show/{id}
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            Pizza pizza = result.get();
+            model.addAttribute("pizza", pizza);
+            return "Pizza/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
+    }
 
 
 }
